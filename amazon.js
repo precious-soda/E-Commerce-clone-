a8eeba6d789ca0756bf3ccfc14d12170c2170335
precious soda <110712195+SUP-SODA@users.noreply.github.com>
2023-08-01 12:37:51 +0530

    Modified cart icon code

--- amazon.js
+++ amazon.js
@@ -74,6 +74,14 @@ document.querySelectorAll('.js-add-to-card').forEach((button) => {
       });
     }
 
-    console.log(cart);
-  })
-})
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+  });
+});
+
+
