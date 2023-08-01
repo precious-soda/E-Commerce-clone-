a9a54323a75e1a921f0f891ab28572163e81b8d1
precious soda <110712195+SUP-SODA@users.noreply.github.com>
2023-08-01 11:34:41 +0530

    Using product.js for products data

--- amazon.js
+++ amazon.js
@@ -1,37 +1,10 @@
-const products = [{
-  img: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 45,
-    count: 87
-  },
-  priceCents: 1090
-}, {
-  img: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 40,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  img: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 45,
-    count: 56
-  },
-  priceCents: 799
-}
-];
-
 let productsHTML = '';
 
 products.forEach((product) => {
   productsHTML += `
      <div class="product-container">
         <div class="product-image-container">
-          <img class="product-image" src="${product.img}">
+          <img class="product-image" src="${product.image}">
         </div>
 
         <div class="product-name limit-text-to-2-lines">
@@ -39,7 +12,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-rating-container">
-          <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars}.png">
+          <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
           <div class="product-rating-count link-primary">${product.rating.count}
           </div>
         </div>
