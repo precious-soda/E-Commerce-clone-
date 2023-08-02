d7dbd37afe844019691ac4430a1c7cc9468d3fb9
precious soda <110712195+SUP-SODA@users.noreply.github.com>
2023-08-02 11:07:54 +0530

    Refactoring: Making code modular

--- amazon.js
+++ amazon.js
@@ -1,4 +1,7 @@
-let productsHTML = '';
+import { cart, addToCart } from '../data/cart.js';
+import { products } from '../data/products.js';
+
+export let productsHTML = '';
 
 products.forEach((product) => {
   productsHTML += `
@@ -54,33 +57,22 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((item) => {
+    cartQuantity += item.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-card').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
-    let matchingItem;
+    addToCart(productId);
+    updateCartQuantity();
 
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    })
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId,
-        quantity: 1
-      });
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
 
